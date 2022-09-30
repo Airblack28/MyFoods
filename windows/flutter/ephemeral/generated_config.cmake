c240dca472ae79ee89c9e676b11878ce9d512ff0
Ryan Arthur <86264705+Airblack28@users.noreply.github.com>
2022-09-30 20:41:02 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\SRC\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\myfoods" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\SRC\\flutter"
+  "PROJECT_DIR=E:\\myfoods"
+  "FLUTTER_ROOT=C:\\SRC\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\myfoods\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\myfoods"
+  "FLUTTER_TARGET=E:\\myfoods\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\myfoods\\.dart_tool\\package_config.json"
+)
